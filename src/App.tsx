d26d2c2f0295c263f3eb73fa50ca2251f68c4c76
Kai <b10223052@gmail.com>
2024-12-03 15:34:31 +0800

    feat: add demo component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import { Button } from '@/components/ui/button';
+
 function App() {
   return (
-    <div className="w-full h-full flex items-center justify-center">
+    <div className="w-full h-full flex items-center justify-center flex-col gap-4">
       <h1 className="text-3xl font-bold underline text-red-300">
         Hello world!
       </h1>
+      <Button>Click Me</Button>
     </div>
   );
 }
