677f589a4857582e755744da69cb7499423e3142
Kai <b10223052@gmail.com>
2024-12-11 14:56:30 +0800

    feat: add data and calc

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,67 +1,166 @@
 import { Button } from '@/components/ui/button';
-import ModeToggle from '@/components/ModeToggle';
 import { calculateSemiAnnualCompoundAssets } from '@/lib/core';
 import { Input } from '@/components/ui/input';
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
-  FormMessage,
 } from '@/components/ui/form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
+import Layout from '@/components/Layout';
+import { useState } from 'react';
 
 const FormSchema = z.object({
-  username: z.string().min(2, {
-    message: 'Username must be at least 2 characters.',
-  }),
+  currentAge: z.string(),
+  retirementAge: z.string(),
+  monthlySavings: z.string(),
+  monthlyInvestment: z.string(),
+  annualReturnRate: z.string(),
 });
 
 function App() {
+  const [result, setResult] =
+    useState<ReturnType<typeof calculateSemiAnnualCompoundAssets>>();
   const form = useForm<z.infer<typeof FormSchema>>({
     resolver: zodResolver(FormSchema),
     defaultValues: {
-      username: '',
+      currentAge: '20',
+      retirementAge: '65',
+      monthlySavings: '1000',
+      monthlyInvestment: '1000',
+      annualReturnRate: '5',
     },
   });
 
   function onSubmit(data: z.infer<typeof FormSchema>) {
     console.log(data);
-    console.log(calculateSemiAnnualCompoundAssets(20, 65, 1000, 1000, 5));
+    console.log(
+      calculateSemiAnnualCompoundAssets(
+        parseInt(data.currentAge),
+        parseInt(data.retirementAge),
+        parseInt(data.monthlySavings),
+        parseInt(data.monthlyInvestment),
+        parseInt(data.annualReturnRate)
+      )
+    );
+    setResult(
+      calculateSemiAnnualCompoundAssets(
+        parseInt(data.currentAge),
+        parseInt(data.retirementAge),
+        parseInt(data.monthlySavings),
+        parseInt(data.monthlyInvestment),
+        parseInt(data.annualReturnRate)
+      )
+    );
   }
 
   return (
-    <div className="w-full h-full flex items-center justify-center flex-col gap-4">
-      <Form {...form}>
-        <form
-          onSubmit={form.handleSubmit(onSubmit)}
-          className="w-2/3 space-y-6"
-        >
-          <FormField
-            control={form.control}
-            name="username"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Username</FormLabel>
-                <FormControl>
-                  <Input placeholder="shadcn" {...field} />
-                </FormControl>
-                <FormDescription>
-                  This is your public display name.
-                </FormDescription>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <Button type="submit">Submit</Button>
-        </form>
-      </Form>
-      <ModeToggle />
-    </div>
+    <Layout>
+      <div className="w-full h-full flex items-start justify-center gap-6  max-w-screen-xl mx-auto px-4">
+        <div className="w-96 flex-shrink-0">
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="w-full space-y-6"
+            >
+              <FormField
+                control={form.control}
+                name="currentAge"
+                render={({ field }) => (
+                  <FormItem className="flex items-center gap-2 space-y-0">
+                    <FormLabel className="flex-shrink-0 w-24">
+                      現在年紀
+                    </FormLabel>
+                    <FormControl>
+                      <Input placeholder="18" {...field} />
+                    </FormControl>
+                    歲
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="retirementAge"
+                render={({ field }) => (
+                  <FormItem className="flex items-center gap-2 space-y-0">
+                    <FormLabel className="flex-shrink-0 w-24">
+                      退休年齡
+                    </FormLabel>
+                    <FormControl>
+                      <Input placeholder="65" {...field} />
+                    </FormControl>
+                    歲
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="monthlySavings"
+                render={({ field }) => (
+                  <FormItem className="flex items-center gap-2 space-y-0">
+                    <FormLabel className="flex-shrink-0 w-24">
+                      每月儲蓄金額
+                    </FormLabel>
+                    <FormControl>
+                      <Input placeholder="1000" {...field} />
+                    </FormControl>
+                    元
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="monthlyInvestment"
+                render={({ field }) => (
+                  <FormItem className="flex items-center gap-2 space-y-0">
+                    <FormLabel className="flex-shrink-0 w-24">
+                      每月投資金額
+                    </FormLabel>
+                    <FormControl>
+                      <Input placeholder="1000" {...field} />
+                    </FormControl>
+                    元
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="annualReturnRate"
+                render={({ field }) => (
+                  <FormItem className="flex items-center gap-2 space-y-0">
+                    <FormLabel className="flex-shrink-0 w-24">
+                      年化報酬率
+                    </FormLabel>
+                    <FormControl>
+                      <Input placeholder="5" {...field} />
+                    </FormControl>
+                    %
+                  </FormItem>
+                )}
+              />
+              <div className="text-right">
+                <Button type="submit">Submit</Button>
+              </div>
+            </form>
+          </Form>
+        </div>
+        <div className="flex-1 overflow-y-scroll h-[calc(100vh-100px)]">
+          {result?.map((item) => (
+            <div key={item.month} className="flex items-center gap-4">
+              <div>第 {item.month} 個月</div>
+              <div>總資產：{item.totalAssets.toFixed(2)}</div>
+              <div>儲蓄金額：{item.totalSavings.toFixed(2)}</div>
+              <div>投資金額：{item.totalInvestments.toFixed(2)}</div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </Layout>
   );
 }
 
